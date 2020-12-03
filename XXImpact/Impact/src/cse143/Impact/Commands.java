cd2561dcd00f101f893c2341e83774df0b1f7bb5
brandonawds <bzl@uw.edu>
2020-12-02 21:54:05 -0800

    added .jar in repo

--- XXImpact/Impact/src/cse143/Impact/Commands.java
+++ XXImpact/Impact/src/cse143/Impact/Commands.java
@@ -10,13 +10,15 @@ public class Commands extends ListenerAdapter {
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		// Getting the argument/commands
 		String[] args = event.getMessage().getContentRaw().split(" ");
+		
 		// If !info, bot will send back information about itself
 		if (args[0].equalsIgnoreCase(Main.prefix + "info")) {
 			EmbedBuilder info = new EmbedBuilder();
 			info.setTitle("🎰 XXImpact Bot Information");
 			info.setDescription("XX Impact (real name TBD) is a Discord bot based on the gacha game system. A player pays a specific amount of currency to “spin” the gacha, and then receives a random item or character. Don’t worry, we won’t actually make you spend real money on waifus/husbandos. It will let Discord users join the game through inputted commands allowing them to gain points, spend those points on rolls for characters, keep track of their roster & character stats; further allowing them to potentially battle other friends. ");
-			info.addField("Creators", "Brandon Ly, Vivian Nguyen, Vincent Le", false);
+			//info.addField("Creators", "Brandon Ly, Vivian Nguyen, Vincent Le", false);
 			info.setColor(0xa2defc);
+			info.setFooter("Created by Brandon Ly, Vivian Nguyen, Vincent Le", event.getMember().getUser().getAvatarUrl());
 			
 			// Shows that bot is typing in discord
 			event.getChannel().sendTyping().queue();
