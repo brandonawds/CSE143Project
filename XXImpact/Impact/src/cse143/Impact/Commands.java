305af67796b4fed522f634c17bda75bc0162dc42
brandonawds <bzl@uw.edu>
2020-12-02 21:38:18 -0800

    Adding some commands

---
+++ XXImpact/Impact/src/cse143/Impact/Commands.java
@@ -0,0 +1,35 @@
+package cse143.Impact;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Commands extends ListenerAdapter {
+	
+	// onGuildMessageReceived looks for commands/events and will send things back to the channel
+	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+		// Getting the argument/commands
+		String[] args = event.getMessage().getContentRaw().split(" ");
+		// If !info, bot will send back information about itself
+		if (args[0].equalsIgnoreCase(Main.prefix + "info")) {
+			EmbedBuilder info = new EmbedBuilder();
+			info.setTitle("🎰 XXImpact Bot Information");
+			info.setDescription("XX Impact (real name TBD) is a Discord bot based on the gacha game system. A player pays a specific amount of currency to “spin” the gacha, and then receives a random item or character. Don’t worry, we won’t actually make you spend real money on waifus/husbandos. It will let Discord users join the game through inputted commands allowing them to gain points, spend those points on rolls for characters, keep track of their roster & character stats; further allowing them to potentially battle other friends. ");
+			info.addField("Creators", "Brandon Ly, Vivian Nguyen, Vincent Le", false);
+			info.setColor(0xa2defc);
+			
+			// Shows that bot is typing in discord
+			event.getChannel().sendTyping().queue();
+			// Builds and sends the message
+			event.getChannel().sendMessage(info.build()).queue();
+			// Clears the embed message for storage sake
+			info.clear();
+		}
+		
+		// If !ayy command, bot will respond with lmao
+		if (args[0].equalsIgnoreCase(Main.prefix + "ayy")) {
+			event.getChannel().sendTyping().queue();
+			event.getChannel().sendMessage("lmao").queue();
+		}		
+	}
+}
--- XXImpact/Impact/src/cse143/Impact/Main.java
+++ XXImpact/Impact/src/cse143/Impact/Main.java
@@ -1,8 +1,7 @@
 package cse143.Impact;
 
+// Imports
 import javax.security.auth.login.LoginException;
-
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
@@ -10,14 +9,21 @@ import net.dv8tion.jda.api.entities.Activity;
 
 public class Main {
 	public static JDA jda;
+	public static String prefix = "!";
+	
 	
 	// Main Method
 	public static void main(String[] args) throws LoginException {
-		jda = new JDABuilder(AccountType.BOT).setToken("NzgzNTU4NDQzNTE5NzA1MDg4.X8cfqg._Rdt7ZMFLK_8rHWeVDqdjg5qyMQ").build();
+		// Creating the bot with the token they gave me in discord dev portal
+		jda = JDABuilder.createDefault("NzgzNTU4NDQzNTE5NzA1MDg4.X8cfqg._Rdt7ZMFLK_8rHWeVDqdjg5qyMQ").build();
+		
 		// Setting bot as idle
 		jda.getPresence().setStatus(OnlineStatus.IDLE);
+		
 		// Setting bot activity with setActivity(Activity.playing/watching/etc)
 		jda.getPresence().setActivity(Activity.playing("VALORANT"));
-	
+		
+		// Telling bot to listen to commands
+		jda.addEventListener(new Commands());
 	}
 }
